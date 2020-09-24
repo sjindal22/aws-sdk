502dcda854006e9399c3f3f8a085b05ec2af15be
Shivika <shivika.jindal01@gmail.com>
2020-09-24 12:34:21 -0400

    split operations into functions for more readibility

--- main.go
+++ main.go
@@ -8,17 +8,44 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
-func main() {
+func createSession(r string, prof string) *session.Session {
 
-	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String("us-east-1")}))
-	ssmSess := ssm.New(sess)
-	op, err := ssmSess.GetParameter(&ssm.GetParameterInput{
-		Name:           aws.String("shivika"),
-		WithDecryption: aws.Bool(false),
-	})
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String(r),
+		},
+		Profile: prof,
+	},
+	)
 	if err != nil {
 		fmt.Println(err)
 	}
-	value := *op.Parameter.Value
-	fmt.Println(value)
+	return sess
+}
+
+/*func createSSMParameters() {
+
+}
+*/
+
+func getSSMParameters(r string, p string) {
+
+	paramsList := []string{"/testaccount/env/dev", "foo"}
+	strPtr := aws.StringSlice(paramsList)
+	//csess := ssm.New(createSession(), aws.NewConfig.WithRegion("us-east-1"))
+	csess := ssm.New(createSession(r, p))
+	paramOP, err := csess.GetParameters(&ssm.GetParametersInput{Names: strPtr})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, val := range paramOP.Parameters {
+		fmt.Println(*val.Value)
+	}
+
+}
+
+func main() {
+
+	getSSMParameters("us-east-1", "devops")
 }
